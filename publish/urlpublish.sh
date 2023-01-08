fb0faff5dd869ea3b68383a04a1f8ae2b5603260
RonBeny <ronbenyamin@gmail.com>
2023-01-08 09:00:53 +0200

    publish url fix

--- publish/urlpublish.sh
+++ publish/urlpublish.sh
@@ -3,6 +3,7 @@
 # Get the Ngrok url Jq is required 
 url=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[].public_url | select(test("https.*"))')
 
+echo "Public URL is ${url}"
 
 # update the github webhook with the Ngrok url 
 curl \
@@ -10,5 +11,5 @@ curl \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_SECRET}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/${GITHUB_REPO_JOB_DSL}/Aurora/hooks \
+  https://api.github.com/repos/${GITHUB_REPO_JOB_DSL}/hooks \
   -d "{\"name\":\"web\",\"active\":true,\"events\":[\"pull_request\"],\"config\":{\"url\":\"$url/github-webhook/\",\"content_type\":\"json\",\"insecure_ssl\":\"0\"}}"
